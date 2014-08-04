f7148523ab20029173e1d8ae336ac9419bc13393
Sean Kellman <stkellman@gmail.com>
2014-08-04 19:54:40 -0400

    adding partial and related

---
+++ example_user.rb
@@ -0,0 +1,12 @@
+class User  #this user class was added at 4.4.5
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+end
